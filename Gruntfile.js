689f57e09ef5e2d2ed4309d681320b0fc104b690
Udo Kramer <info@udokramer.com>
2013-03-16 03:30:32 +0100

    use 4 space for indentation in Gruntfile and enable indent (4) and trailing option for jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,18 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-    	all: ['src/**/*.js']
-  	}
-  });
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            options: {
+                indent: 4,
+                trailing: true
+            },
+            all: ['src/**/*.js']
+        }
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.registerTask('default', ['jshint']);
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.registerTask('default', ['jshint']);
 
 };
